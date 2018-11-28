5407caa9f00bc786881b9467569daeef20cbef21
pravaibh <vaibhav.prateek7@gmail.com>
2018-11-29 01:17:04 +0530

    changed files for ip

--- src/app/service/rest.service.ts
+++ src/app/service/rest.service.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 
 
-const endpoint = 'http://localhost:8080/register';
+const endpoint = 'http://ec2-54-89-207-214.compute-1.amazonaws.com:8080/register';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
